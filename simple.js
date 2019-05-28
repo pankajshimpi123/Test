d6d141d9a6792fb1d26d4b3476239d0a6d2cf4e3
pankajshimpi123 <51022939+pankajshimpi123@users.noreply.github.com>
2019-05-28 20:06:53 +0530

    Update simple.js

--- simple.js
+++ simple.js
@@ -1,10 +1,22 @@
-var http=require('http');
-var url=require('url');
 
-http.createServer(function(req,res)
+var http=require('http');   // module
+//object----- model
+var product ={
+    title:"Rose",
+    description:"Valentine Flower",
+    unitprice:25,
+    likes:500,
+    quantity:5000
+};
+
+var count=45;
+count++;
+var controller=function(request,response)
 {
- res.write("<h1 style='color:red'> Welcome to Node Server </h1>");
-res.end();
-
-}).listen(9000);
-console.log("server started at 9000");
+	response.writeHead(200,
+	                {"Content-Type" : "text/plain"});
+	                response.end( JSON.stringify(product));
+};
+var server=http.createServer(controller);
+server.listen(7000);
+console.log("listening on port 7000");
